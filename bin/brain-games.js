42764d8c7b151810f44a0ccd8fa7e5d27290e181
ian iusov <yusov.yan@icloud.com>
2021-03-18 20:52:14 +0300

    step 5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
+import runEngine from '../src/index.js';
 
-greeting();
+runEngine();
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import readlineSync from 'readline-sync';
+
+const numberOfRounds = 3;
+
+const runEngine = (gameData) => {
+
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  console.log(gameData.gameRules);
+
+  const playRound = (currentRound) => {
+    if (currentRound > numberOfRounds) {
+      return true;
+    }
+
+    const { question, correctAnswer } = gameData.getGameQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+
+      return playRound(currentRound + 1);
+    }
+
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    return false;
+  }
+
+  const isWinner = playRound(1);
+  if (isWinner) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}!`);
+  }
+}
+
+export default runEngine;
